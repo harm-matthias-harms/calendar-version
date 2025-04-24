bf91a7d975e0bc23e37ad16de4e2b52d202b9c10
Harm Matthias Harms <matthias.harms@quis.de>
2025-04-24 10:29:05 +0200

    Improve parseLatestVersion test (#74)

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -4,13 +4,13 @@ import { Version } from '../src/version.js'
 
 describe('parseLatestVersion', () => {
   it('returns latest version for tags', () => {
-    expect(parseLatestVersion('', ['1.0.0', '1.0.1', '1.1.0', '1.01.0'])).toEqual(new Version(1, 1, 0))
+    expect(parseLatestVersion('', ['1.0.0', '1.0.1', '1.1.0', '1.02.0'])).toEqual(new Version(1, 2, 0))
   })
   it('filters tags by prefix', () => {
-    expect(parseLatestVersion('v', ['v1.0.0', '1.0.1', '1.1.0', '1.01.0'])).toEqual(new Version(1, 0, 0))
+    expect(parseLatestVersion('v', ['v1.0.0', '1.0.1', '1.1.0'])).toEqual(new Version(1, 0, 0))
   })
   it('handles invalid tags', () => {
-    expect(parseLatestVersion('', ['1.0.0', '1.0.1', '1.00.1', '1.1', 'invalid'])).toEqual(new Version(1, 0, 1))
+    expect(parseLatestVersion('', ['1.0.0', '1.0.1', '1.01.1', '1.1', 'invalid'])).toEqual(new Version(1, 1, 1))
   })
   it('returns latest version for empty list of tags', () => {
     expect(parseLatestVersion('', [])).toEqual(new Version(0, 0, 0))
